e96230a98ef0de7ef8f3555d4e1cafb72d33ba7f
Roshan Adhikari <nahsorad@gmail.com>
2021-05-19 21:24:37 +0545

    Initial commit

---
+++ src/test/java/np/com/roshanadhikary/mdblog/MdBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package np.com.roshanadhikary.mdblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MdBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
